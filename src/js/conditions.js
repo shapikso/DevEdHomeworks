268154b26bd844c489ae02545da6234ed7934aec
shapikso <79461623+shapikso@users.noreply.github.com>
2021-10-30 16:46:50 +0300

    done git initiation

---
+++ src/js/conditions.js
@@ -0,0 +1,76 @@
+const binary = (a,b) => {
+    if((a % 2) === 0) {
+      return a*b;
+    }
+    else {  
+      return a+b
+    }
+  }
+  
+  console.log(binary(2,3))
+  
+  const axis = (x,y) => {
+  
+    switch (true) {
+      case x > 0 && y > 0:
+        console.log('Первая четверть')
+      break
+      case x < 0 && y > 0:
+        console.log('Вторая четверть')
+      break
+      case x < 0 && y < 0:
+        console.log('Третья четверть')
+      break
+         
+      default:
+        console.log( 'Четвертая четверть' );
+      }
+  }
+  axis(-2,-5)
+  
+  const sumPositive = (a,b,c) => {
+    let arr = [a,b,c]
+    let sum = 0
+    arr.forEach( elem => {
+      if(elem > 0) {
+        sum = sum + elem
+      }
+    }) 
+    return sum
+  }
+  console.log(sumPositive(-1,2,3))
+  
+  const sumMultMax = (a,b,c) =>{ 
+    if((a+b+c)>(a*b*c)){
+      return (a+b+c)+3
+    }
+    else {
+      return (a*b*c)+3
+    }
+  }
+  console.log(sumMultMax(1,0,3))
+  
+  const mark = (mark) => {
+    switch (true) {
+      case mark > 19 && mark < 40:
+        console.log('E')
+      break
+      case mark > 39 && mark < 60:
+        console.log('D')
+      break
+      case mark > 59 && mark < 75:
+        console.log('C')
+      break
+      case mark > 74 && mark < 90:
+        console.log('B')
+      break
+      case mark > 89 && mark < 101:
+        console.log('A')
+      break
+         
+      default:
+        console.log( 'D' );
+      }
+  
+  }
+  mark(75)
