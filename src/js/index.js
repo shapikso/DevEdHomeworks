2421a79165ffe99b90932601ed22ba0be62c80ec
shapikso <79461623+shapikso@users.noreply.github.com>
2021-11-15 23:03:13 +0200

    done firs two tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,198 +1,125 @@
-// Условные операторы
-
-const binary = (a,b) => {
-  if((a % 2) === 0) {
-    return a*b;
-  }
-  else {  
-    return a+b
-  }
-}
-
-console.log(binary(2,3))
-
-const axis = (x,y) => {
-
-  switch (true) {
-    case x > 0 && y > 0:
-      console.log('Первая четверть')
-    break
-    case x < 0 && y > 0:
-      console.log('Вторая четверть')
-    break
-    case x < 0 && y < 0:
-      console.log('Третья четверть')
-    break
-       
-    default:
-      console.log( 'Четвертая четверть' );
+const tryToGiveChange = (array,change,money) => {
+    let changeKoefficient = change / 25
+    while (change !== 0 && changeKoefficient !== 0) {
+        if(array.includes(25*changeKoefficient)){
+            array.splice(array.indexOf(25*changeKoefficient), 1);
+            change = change-(25*changeKoefficient);
+        }
+        else {
+            changeKoefficient--;
+        }
     }
+    array.push(money);
+    return change === 0 ? array : undefined;
 }
-axis(-2,-5)
-
-const sumPositive = (a,b,c) => {
-  let arr = [a,b,c]
-  let sum = 0
-  arr.forEach( elem => {
-    if(elem > 0) {
-      sum = sum + elem
+const tickets = (person) => {
+    const cashbox = person.reduce((acc, money)=>{
+        if ( acc && money === 25 )
+        {
+            return [...acc,money];
+        }
+        else if (acc && money > 25 && money % 25 === 0){
+            return tryToGiveChange(acc,money - 25,money);
+        }
+        else {
+            return undefined;
+        }
+    },[])
+    return cashbox ? 'YES': 'NO'
+}
+console.log(tickets([26, 25, 50, 25 ,100]));
+const k = []
+//console.log( k[3] )
+const sum = (arrayNumberOne,arrayNumberTwo) => {
+    const result = []
+    result[arrayNumberOne.length-1] = 0;
+    for (let i = arrayNumberOne.length-1; i >= 0; i--) {
+        result[i] = result[i] + Number(arrayNumberOne[i]) + Number(arrayNumberTwo[i]);
+        if (i !== 0 && result[i] > 9) {
+            result[i] = result[i] - 10;
+            result[i-1] = 1; 
+        }
+        else if(i !== 0 ){
+            result[i-1] = 0;
+        }
     }
-  }) 
-  return sum
+ return result.join('');
 }
-console.log(sumPositive(-1,2,3))
-
-const sumMultMax = (a,b,c) =>{ 
-  if((a+b+c)>(a*b*c)){
-    return (a+b+c)+3
-  }
-  else {
-    return (a*b*c)+3
-  }
-}
-console.log(sumMultMax(1,0,3))
-
-const mark = (mark) => {
-  switch (true) {
-    case mark > 19 && mark < 40:
-      console.log('E')
-    break
-    case mark > 39 && mark < 60:
-      console.log('D')
-    break
-    case mark > 59 && mark < 75:
-      console.log('C')
-    break
-    case mark > 74 && mark < 90:
-      console.log('B')
-    break
-    case mark > 89 && mark < 101:
-      console.log('A')
-    break
-       
-    default:
-      console.log( 'D' );
+const getSum = (firstNumber,secondNumber) => {
+    arrayNumberOne = Array.from(firstNumber);
+    arrayNumberTwo = Array.from(secondNumber);
+    while (arrayNumberOne.length !== arrayNumberTwo.length) {
+        if (arrayNumberOne.length > arrayNumberTwo.length){
+            arrayNumberTwo.unshift('0');
+        }
+        else {
+            arrayNumberOne.unshift('0');
+        }
     }
-
+    return sum(arrayNumberOne,arrayNumberTwo)
 }
-mark(75)
+console.log(getSum('923', '123'));
 
-//Циклы
-
-
-const sumBinary = () => {
-  let sum = 0
-  let count = 0;
-  for(let i = 2; i < 99; i+=2) {
-    sum = sum +i;
-    count++
-  }
-  console.log(`Cумма всех четных элементов от 1 до 99 равна: ${sum}, а их число равно: ${count}`)
-}
-sumBinary()
-
-const isPrimeNumber = (number) => {
-  number = Math.abs(number)
-  for(let i = 2; i < number; i++) {
-    if(number % i === 0) { 
-      return true
+let listOfPosts2 = [
+    {
+    id: 1,
+    post:'some post1',
+    title: 'title 1',
+    author:'Ivanov',
+    comments: [
+    {
+    id: 1.1,
+    comment: 'some comment1',
+    title: 'title',
+    author: 'Rimus'
+    },
+    {
+    id: 1.2,
+    comment: 'some comment2',
+    title: 'title' ,
+    author: 'Uncle'
     }
-  }
-  return false
-}
-console.log(isPrimeNumber(-16))
-
-const findFactorial = (number) => {
-  let fact = 1
-  for(let i = 2; i < number; i++) {
-    fact = fact * i
-  }
-  console.log(`Факториал числа ${number} равен: ${fact}`)
-}
-findFactorial(10)
-
-const numberSum = (number) => {
-  number = String(number)
-  let sum = 0
-  for(let i = 0; i < number.length; i++) {
-    sum = sum + +number[i]
-  }
-  console.log(`Сумма цифр числа ${number} равна: ${sum}`)
-}
-numberSum(1113)
-
-const numberMirror = (number) => {
-  number = String(number)
-  let sum = ''
-  for(let i = number.length-1; i >= 0; i--) {
-    sum = sum + number[i]
-  }
-  console.log(`Зеркальное отображение числа ${number} равна: ${sum}`)
-}
-numberMirror(123)
-
-
-// Массивы
-const randomArr = [3,2,8,1,56,9,72,30,66,14,99,11,88,65,69,54,77,-10,-75,-69,-90,46,1,12,7,36,-12] 
-
-const findMax = (arr) => {
-  let max = arr[0]
-  arr.forEach( el => {
-    if (el > max) {
-      max = el
+    ]
+    },
+    {
+    id: 2,
+    post: 'some post2',
+    title: 'title',
+    author: 'Ivanov',
+    comments: [
+    {
+    id: 1.1,
+    comment: 'some comment1',
+    title: 'title 1',
+    author: 'Rimus'
+    },
+    {
+    
+    id: 1.2,
+    comment:'some comment2',
+    title: 'title 2',
+    author: 'Uncle'
+    },
+    {
+    id: 1.3,
+    comment: 'some comment3',
+    title: 'title 3',
+    author: 'Rimus'
     }
-  })
-  return max
-}
-console.log('Максимальный элемент массива ' + findMax(randomArr)) 
-
-const findMin = (arr) => {
-  let min = arr[0]
-  arr.forEach( el => {
-    if (el < min) {
-      min = el
+    ]
+    },
+    {
+    id: 3,
+    post: 'some post3',
+    title:'title 3',
+    author: 'Rimus'
+    },
+    {
+    id: 4,
+    post: 'some post4',
+    title: 'title 4',
+    author: 'Uncle'
     }
-  })
-  return min
-}
-console.log('Минимальный элемент массива ' + findMin(randomArr))
+    ]
 
-const findMaxIndex = (arr) => {
- return arr.indexOf(findMax(arr))+1
-}
-console.log('Индекс максимального элементa массива ' + findMaxIndex(randomArr))
-
-const findMinIndex = (arr) => {
-  return arr.indexOf(findMin(arr))+1
-}
-console.log('Индекс минимального элементa массива ' + findMinIndex(randomArr))
-
-const sumNotBinary = (arr) => {
-  let sum = 0
-  for (let i = 1; i<arr.length; i+=2) {
-    sum = sum + arr[i]
-  }
-  return sum
-}
-console.log('Cуммa элементов массива с нечетными индексами ' + sumNotBinary(randomArr))
-
-const reversArr = (arr) => {
-  let reversedArray =[]
-  arr.forEach(el => {
-    reversedArray.unshift(el)
-  })
-  return reversedArray
-}
-console.log(reversArr(randomArr))
-
-const reversHalf = (arr) => { 
-  let reversedHalf = []
-  for (let i = (arr.length/2).toFixed(0); i < arr.length; i++) {
-    reversedHalf.push(arr[i])
-  }
-  for (let i = 0; i < arr.length/2; i++) {
-    reversedHalf.push(arr[i])
-  }
-   return reversedHalf
-}
-console.log(reversHalf(randomArr))
+    
