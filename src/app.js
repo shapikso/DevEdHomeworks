c0b90473bfd5d28678e3efdbbe0b1f03697eb03c
shapikso <79461623+shapikso@users.noreply.github.com>
2021-11-28 18:21:08 +0200

    Done hw

--- src/app.js
+++ src/app.js
@@ -1,92 +1,75 @@
-// import "../../common/css/normalize.css";
-// import "../../common/css/skeleton.css";
-// import "../../common/css/dark-theme.css";
 import "./styles.css";
 
-const addTaskForm = document.getElementById("addTaskForm");
-const taskNameInput = document.getElementById("taskNameInput");
-const taskList = document.getElementById("taskList");
-const taskItemTemplate = document.getElementById("taskItemTemplate").innerHTML;
-
-addTaskForm.addEventListener("submit", onAddTaskFormSubmit);
-taskList.addEventListener("click", onTaskListClick);
-
-getList();
-
-function getList() {
-  return fetch("https://jsonplaceholder.typicode.com/todos?_limit=15")
-    .then((res) => res.json())
-    .then(renderList);
-}
-
-function renderList(data) {
-  data.forEach(addTask);
-}
-
-function onAddTaskFormSubmit(event) {
-  event.preventDefault();
-
-  submitForm();
-}
-
-function onTaskListClick(event) {
-  switch (true) {
-    case event.target.classList.contains("task-item"):
-      toggleTaskState(event.target);
-      break;
-    case event.target.classList.contains("delete-btn"):
-      deleteTask(event.target.parentElement);
-      break;
+class View {
+  constructor () {
+    
+  }
+  showTask (html){
+    const newTaskEl = this.view.htmlToElement(html);
+    taskList.appendChild(newTaskEl);
+  }
+  htmlToElement(html) {
+    const template = document.createElement("template");
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+  }
+  toggleTaskState(el) {
+    el.classList.toggle("done");
   }
 }
 
-function submitForm() {
-  const task = { title: taskNameInput.value };
-  fetch("https://jsonplaceholder.typicode.com/todos/", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(task),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      task.id = data.id;
-      addTask(task);
+class Model {
+  constructor () {
+    this.taskItemTemplate = document.getElementById("taskItemTemplate").innerHTML;
+  }
+  renderList(data) {
+    data.forEach(element => this.model.addTask.call(this, element));
+  }
+  addTask(task) {
+    const html = this.model.taskItemTemplate
+      .replace("{{title}}", task.title)
+      .replace("{{id}}", task.id);
+    this.view.showTask.call(this,html)
+  }
+  onTaskListClick(event) {
+    switch (true) {
+      case event.target.classList.contains("task-item"):
+        this.view.toggleTaskState(event.target);
+        break;
+      case event.target.classList.contains("delete-btn"):
+        this.model.deleteTask.call(this, event.target.parentElement);
+        break;
+    }
+  }
+  deleteTask(el) {
+    fetch("https://jsonplaceholder.typicode.com/todos/" + el.dataset.todoId, {
+      method: "DELETE",
+    }).then(() => {
+      el.remove();
     });
-
-  resetForm();
+  }
+}
+class Controller {
+  constructor (toDo) {
+    const taskList = document.getElementById("taskList");
+    taskList.addEventListener("click", (event) => {
+      toDo.model.onTaskListClick.call(toDo,event)
+    });
+  }
+  getList() {
+    return fetch("https://jsonplaceholder.typicode.com/todos?_limit=15")
+      .then((res) => res.json())
+      .then(json => this.model.renderList.call(this, json));
+  }
 }
 
-function addTask(task) {
-  const html = taskItemTemplate
-    .replace("{{title}}", task.title)
-    .replace("{{id}}", task.id);
-
-  const newTaskEl = htmlToElement(html);
-  taskList.appendChild(newTaskEl);
+class ToDoList {
+  constructor () {
+    this.view = new View();
+    this.model = new Model();
+    this.controller = new Controller(this);
+    this.controller.getList.call(this);
+  }
 }
-
-function resetForm() {
-  addTaskForm.reset();
-}
-
-function toggleTaskState(el) {
-  el.classList.toggle("done");
-}
-
-function deleteTask(el) {
-  fetch("https://jsonplaceholder.typicode.com/todos/" + el.dataset.todoId, {
-    method: "DELETE",
-  }).then(() => {
-    el.remove();
-  });
-}
-
-function htmlToElement(html) {
-  const template = document.createElement("template");
-  html = html.trim();
-  template.innerHTML = html;
-  return template.content.firstChild;
-}
+const list = new ToDoList();
