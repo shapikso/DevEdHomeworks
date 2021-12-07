219364b1fc218a48dbf50c1ccac02100274e8aaf
shapikso <79461623+shapikso@users.noreply.github.com>
2021-12-07 13:46:04 +0200

    added more functions

--- src/app.js
+++ src/app.js
@@ -1,72 +1,73 @@
  const next = document.getElementById('next')
  const prev = document.getElementById('prev')
  
- function returnParams() {
-    const current = document.querySelector('.current');
-    return {current, next : current.nextElementSibling, prev: current.previousElementSibling}
+ function returnParams(current) {
+  
+    return { next : current.nextElementSibling, prev: current.previousElementSibling}
  }
 
  next.addEventListener('click', () => {
-   const {current,next,prev} = returnParams();
-   current.classList.remove('current');
-   current.classList.add('previous');
+   const current = document.querySelector('.current');
+   const {next,prev} = returnParams(current);
 
-   if(prev){
-    prev.classList.remove('previous');
-    prev.classList.add('hidden');
-   } else {
-      const lastElement = document.querySelector('.photos').lastChild
-      lastElement.previousElementSibling.classList.add('hidden');
-      lastElement.previousElementSibling.classList.remove('previous');
-    }
-
-   if (current.nextElementSibling){
-      next.classList.remove('next');
-      next.classList.add('current');
-    } else {
-      const startElement = document.querySelector('.photos').firstChild
-      startElement.nextElementSibling.classList.remove('next');
-      startElement.nextElementSibling.classList.add('current');
-    }
-    if (next?.nextElementSibling){
-      next.nextElementSibling.classList.remove('hidden');
-      next.nextElementSibling.classList.add('next');
-    } else {
-      const startElement = document.querySelector('.photos').firstChild
-      if (startElement.nextElementSibling.classList.contains('current')){
-        startElement.nextElementSibling.nextElementSibling.classList.remove('hidden');
-        startElement.nextElementSibling.nextElementSibling.classList.add('next');
-      } else {
-        startElement.nextElementSibling.classList.remove('hidden');
-        startElement.nextElementSibling.classList.add('next');
-      }
-    }
-   
-
+   makePreviousNext(current, 'previous', 'current');
+   hiddeElement(prev,'hidden', 'previous');
+   makeCurrentElement(next, 'current', 'next');
+   makeNextRight(next,'next', 'hidden')
  })
-
-
  prev.addEventListener('click', () => {
-  const nextPhoto = document.querySelector('.next');
-  const currentPhoto = document.querySelector('.current');
-  const previousPhoto = document.querySelector('.previous');
+  const current = document.querySelector('.current');
+  const {next,prev} = returnParams(current);
 
-  currentPhoto.classList.remove('current');
-  currentPhoto.classList.add('next');
-  previousPhoto.classList.remove('previous');
-  previousPhoto.classList.add('current');
-
-  nextPhoto.classList.remove('next');
-  nextPhoto.classList.add('hidden');
-   if (previousPhoto.previousElementSibling){
-    previousPhoto.previousElementSibling.classList.remove('hidden');
-    previousPhoto.previousElementSibling.classList.add('previous');
-   } else {
-     const lastElement = document.querySelector('.photos').lastChild
-     lastElement.previousElementSibling.classList.remove('hidden');
-     lastElement.previousElementSibling.classList.add('previous');
-
-   }
+  makePreviousPrev(current, 'next', 'hidden');
+  hiddeElement(next,'hidden', 'next');
+  makeCurrentElement(prev, 'current', 'previous');
+  makeNextRight(current,'next', 'current')
 })
+
+ function addRemoveClasses(el, add, remove) {
+   el.classList.add(add);
+   el.classList.remove(remove);
+ }
+ function hiddeElement(el,add,remove) {
+  if(el){
+    addRemoveClasses(el, add, remove);
+  } else {
+     const lastElement = document.querySelector('.photos').lastChild;
+     addRemoveClasses(lastElement.previousElementSibling, add, remove);
+   }
+ }
+ function makeCurrentElement(el, add, remove) {
+  if (el){
+    addRemoveClasses(el, add, remove);
+  } else {
+    const startElement = document.querySelector('.photos').firstChild
+    addRemoveClasses(startElement.nextElementSibling, add, remove);
+  }
+ }
+function makeNextRight(el, add, remove) {
+  if (el?.nextElementSibling){
+    addRemoveClasses(el.nextElementSibling, add, remove);
+  } else {
+    const startElement = document.querySelector('.photos').firstChild
+    if (startElement.nextElementSibling.classList.contains('current')){
+      addRemoveClasses(startElement.nextElementSibling.nextElementSibling, add, remove);
+    } else {
+      addRemoveClasses(startElement.nextElementSibling, add, remove);
+    }
+  }
+}
+function makePreviousNext(el, add, remove) {
+  addRemoveClasses(el, add, remove);
+ }
+
+ function makePreviousPrev(el, add, remove) {
+    addRemoveClasses(el, add, remove);
+    
+ }
+
+
+
+ 
  //nextElementSibling
  //previousElementSibling
