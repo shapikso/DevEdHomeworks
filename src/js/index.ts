b9104cbc4b8f3896d67d931a322142b1de42c154
shapikso <shapovalov.lit@gmail.com>
2021-12-15 13:25:39 +0200

    one weather

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,9 +1,72 @@
 import "../styles.css";
 
-console.log('ts');
+// enum  WeatherEnum{
+//     apiKey = 'f1d53554aa15ea3a40648ba1d31a2e2e',
+//     defaultCity = 'London'
+// }
+// type TCityWeather = {
 
-function add(a:number, b:number): number {
-    return a + b;
+// }
+const mainWeather: HTMLElement | null = document.getElementById("main");
+const select: HTMLElement | null = document.querySelector(".selector");
+const taskItemTemplate = document.getElementById("taskItemTemplate")?.innerHTML;
+
+require("babel-core/register");
+require("babel-polyfill");
+
+function returnImg(imgId : string):string {
+    return `https://openweathermap.org/img/wn/${imgId}@2x.png`
 }
 
-console.log(add(10,20));
+function fromKelToCel (kel:number):string {
+    return (Math.round(kel-273.15)).toString()
+}
+
+function addTask(city:TWeather) {
+    if(taskItemTemplate){
+        const html: string = taskItemTemplate
+        .replace("{{tempNow}}",fromKelToCel(city.main.temp))
+        .replace("{{tempFeel}}", fromKelToCel(city.main.feels_like))
+        .replace("{{cityName}}", city.name)
+        .replace("{{imgId}}",returnImg(city.weather[0].icon));
+    
+        const newTaskEl = htmlToElement(html);
+        if (newTaskEl && mainWeather){
+            mainWeather.appendChild(newTaskEl);
+        }
+    }
+  }
+
+function htmlToElement(html : string) {
+  const template: HTMLTemplateElement = document.createElement("template");
+  html = html.trim();
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+  
+type TWeather = {
+    weather : {id: number,main: string,description: string, icon: string}[],
+    main : {temp: number, feels_like: number,temp_min: number,temp_max: number,pressure: number,humidity: number},
+    wind : {speed: number, deg: number},
+    clouds: {all:number},
+    name: string
+
+}
+
+async function getWeather(
+    URL: string,
+  ): Promise<TWeather> {
+    const response = await fetch(URL);
+    const body = await response.json();
+    return body;
+}
+
+//getWeather('https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=f1d53554aa15ea3a40648ba1d31a2e2e').then((data: TWeather) => addTask(data))
+
+select?.addEventListener('change', (event):void => {
+    console.log('in select');
+    //onst elem = event.target
+    if(event.target.value){
+    getWeather(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=f1d53554aa15ea3a40648ba1d31a2e2e`)
+}
+})
